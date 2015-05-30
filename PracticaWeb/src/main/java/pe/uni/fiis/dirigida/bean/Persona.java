eee5ff6814b339f266767ca32ad5a1f229842c84
Estephanieph <estephanieph@gmail.com>
2015-05-29 20:30:25 -0500

    practica-dirigida-01

---
+++ PracticaWeb/src/main/java/pe/uni/fiis/dirigida/bean/Persona.java
@@ -0,0 +1,41 @@
+package pe.uni.fiis.dirigida.bean;
+
+/**
+ * Created by Esther on 29/05/2015.
+ */
+public class Persona {
+    private String nombre;
+    private String apellidoPaterno;
+    private String correo;
+
+    public String getDatos(){
+        String datos = null;
+        datos = "Nombre:" + nombre + "Apellido Paterno:" + apellidoPaterno + "Correo:" + correo;
+        return datos;
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+}
